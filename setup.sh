4e405be5e619ab3eff6551aefd9f5c9918962c6b
BioGustav <65023470+BioGustav@users.noreply.github.com>
2024-03-03 15:58:18 +0100

    fixup! fixup! fix adding of repos

--- setup.sh
+++ setup.sh
@@ -74,11 +74,7 @@ max_parallel_downloads=20
 
 # mirrors
 dnf install -y $mirrors
-for repo in $repos do
-
-dnf config-manager --add-repo $repo
-
-done
+dnf config-manager --add-repo $repos
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
