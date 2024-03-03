be5158c9d08c584cd473956c20952efd26ab1ef6
BioGustav <65023470+BioGustav@users.noreply.github.com>
2024-03-03 15:56:07 +0100

    fixup! fix adding of repos

--- setup.sh
+++ setup.sh
@@ -75,7 +75,9 @@ max_parallel_downloads=20
 # mirrors
 dnf install -y $mirrors
 for repo in $repos do
-	dnf config-manager --add-repo $repo
+
+dnf config-manager --add-repo $repo
+
 done
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
