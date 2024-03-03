898c3f6f02dcbb12531f99a2aca9669a7d481fa4
BioGustav <65023470+BioGustav@users.noreply.github.com>
2024-03-03 14:51:15 +0100

    use antigen now for zsh

--- setup.sh
+++ setup.sh
@@ -7,28 +7,53 @@ else
   echo "I am ROOOOOT"
 fi
 
+# config
 mirrors="
 	fedora-workstation-repositories
 	https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+	https://download.opensuse.org/repositories/shells:zsh-users:antigen/Fedora_Rawhide/shells:zsh-users:antigen.repo
 "
 
 packages="
-	micro
+	antigen
 	btop
-	discord
-	java-latest-openjdk
 	code
-	zsh
-	xclip
-	tldr
+	discord
+	gcc
 	gh
-"
+	git
+	java-latest-openjdk
+	neofetch
+	micro
+	mupdf
+	mpv
+	tldr
 #	texlive-scheme-full
+	virt-manager
+	xclip
+	zsh
+"
+
+removed_packages="
+	cheese
+	gnome-boxes
+	gnome-maps
+	gnome-tour
+	gnome-weather
+	libreoffice
+	rhythmbox
+	simple-scan
+	totem
+	yelp
+"
 
 flatpaks="
-	com.spotify.Client
+	com.bitwarden.desktop
 	md.obsidian.Obsidian
+	org.onlyoffice.desktopeditors
+	org.signal.Signal
+	com.spotify.Client
 "
 
 # dnf
@@ -57,34 +82,35 @@ dnf groupupdate -y core
 dnf update -y --refresh
 dnf install -y $packages
 
+dnf remove -y $removed_packages
+
 flatpak update
 flatpak install -y $flatpaks
 sh -c "$(wget https://sh.rustup.rs -O -)" -- -y
 
 # JetBrains toolbox
-wget -cO /tmp/jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
+(
+wget -cqO /tmp/jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
 tar -xzf /tmp/jetbrains-toolbox.tar.gz --directory=/tmp
 /tmp/jetbrains*/jetbrains-toolbox
 rm -r /tmp/jetbrains*
+)&
 
 # fonts
-wget -cO /tmp/CodeNewRoman.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/CodeNewRoman.zip
-unzip /tmp/CodeNewRoman.zip -x README.md license.txt -d ~/.local/share/fonts
+(
+wget -cqO /tmp/CodeNewRoman.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/CodeNewRoman.zip
+unzip -q /tmp/CodeNewRoman.zip -x README.md license.txt -d /home/$SUDO_USER/.local/share/fonts
 rm -r /tmp/CodeNewRoman*
+)&
 
+wait
 # zsh
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" -- --unattended
-
-cp ./poshthemes/* ~/.poshthemes/
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-cp ./zshrc ~/.zshrc
-cp ./aliases ~/.config/
-
 curl -s https://ohmyposh.dev/install.sh | bash -s
+cp ./themes/* /home/$SUDO_USER/.themes/ &
+cp ./zshrc /home/$SUDO_USER/.zshrc &
+cp ./aliases /home/$SUDO_USER/.config/ &
 
-chsh -s $(which zsh)
+wait 
+sudo -u $SUDO_USER chsh -s $(which zsh)
 
 read -p "Press any key to resume ..."
