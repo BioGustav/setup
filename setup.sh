a35ebbf3fbc18b7f483af952b759703754b6b877
BioGustav | Tobias Pilz <pilz.tobias+github@gmail.com>
2024-04-26 02:18:20 +0200

    Major adaptions/improvements
    fixes:
    * only append max_parallel_downloads to dnf.conf
    * setup jetbrains-toolbox for SUDO_USER, not for root
    * same for fonts
    
    script sections added:
    * .asc import links
    * download and decompress Ryujinx into SUDO_USERs ~/Downloads
    
    mirrors removed:
    * fedora-workstation-repositories
    
    packages added:
    * akmod-nvidia
    * bat
    * brave-browser
    * dnf-plugins-core
    * firefox
    * speedtest-cli
    * xorg-x11-drv-nvidia-cuda
    
    packages removed:
    * discord
    
    flatpaks added:
    * com.discordapp.Discord
    * com.github.tchx84.Flatseal
    
    aliases added:
    * cat="bat"

--- setup.sh
+++ setup.sh
@@ -9,19 +9,29 @@ fi
 
 # config
 mirrors="
-	fedora-workstation-repositories
 	https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 "
 
 repos="
 	https://download.opensuse.org/repositories/shells:zsh-users:antigen/Fedora_Rawhide/shells:zsh-users:antigen.repo
+	https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 "
 
+ascs="
+	https://packages.microsoft.com/keys/microsoft.asc
+	https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+"
+
+# remove akmod-nvidia and xorg-x11-drv-nvidia-cuda if not using nvidia
 packages="
+	akmod-nvidia
+	bat
+	brave-browser
 	btop
 	code
-	discord
+	dnf-plugins-core
+	firefox
 	gcc
 	gh
 	git
@@ -32,10 +42,12 @@ packages="
 	mpv
 	neofetch
 	obs-studio
+	speedtest-cli
 	steam
 	tldr
 	virt-manager
 	xclip
+	xorg-x11-drv-nvidia-cuda
 	zsh
 "
 #	texlive-scheme-full
@@ -67,27 +79,23 @@ remove_packages="
 
 flatpaks="
 	com.bitwarden.desktop
+	com.discordapp.Discord
+	com.github.tchx84.Flatseal
+	com.spotify.Client
 	md.obsidian.Obsidian
 	org.onlyoffice.desktopeditors
 	org.signal.Signal
-	com.spotify.Client
 "
 
 # dnf
-echo "[main]
-gpgcheck=True
-installonly_limit=3
-clean_requirements_on_remove=True
-best=False
-skip_if_unavailable=True
-
+echo "
 # custom
 max_parallel_downloads=20
-" > /etc/dnf/dnf.conf
+" >> /etc/dnf/dnf.conf
 
 # mirrors
 dnf install -y $mirrors
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
+rpm --import $ascs
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 dnf config-manager --disable google-chrome
@@ -101,21 +109,34 @@ dnf update -y --refresh
 dnf install -y $packages
 
 
-flatpak update
+flatpak update -y
 flatpak install -y $flatpaks
 sudo -u $SUDO_USER sh -c "wget -qO- https://sh.rustup.rs | sh -s -- -y"
+
+# Ryujinx - just download latest version and extract
+(
+ryujinx_folder="/home/$SUDO_USER/Downloads/ryujinx"
+wget -cqO /tmp/ryujinx.tar.gz $(
+	curl https://api.github.com/repos/Ryujinx/release-channel-master/releases \
+	| jq -r '.[0].assets[].browser_download_url' \
+	| grep -E "/ryujinx-[0-9.]*-linux_x64.tar.gz" \
+)
+sudo -u $SUDO_USER mkdir -p $ryujinx_folder
+sudo -u $SUDO_USER tar -xzf /tmp/ryujinx.tar.gz --directory=$ryujinx_folder
+rm /tmp/ryujinx.tar.gz
+)&
 
 # JetBrains toolbox
 (
 wget -cqO /tmp/jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
 tar -xzf /tmp/jetbrains-toolbox.tar.gz --directory=/tmp
-/tmp/jetbrains*/jetbrains-toolbox
+sudo -u $SUDO_USER /tmp/jetbrains*/jetbrains-toolbox
 rm -r /tmp/jetbrains*
 )&
 
 # fonts
 (
-wget -cqO /tmp/CodeNewRoman.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/CodeNewRoman.zip
+wget -cqO /tmp/CodeNewRoman.zip https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CodeNewRoman.zip
 unzip -q /tmp/CodeNewRoman.zip -x README.md license.txt -d /home/$SUDO_USER/.local/share/fonts
 rm -r /tmp/CodeNewRoman*
 )&
